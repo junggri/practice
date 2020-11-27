20a121e711109ed0d8d69ace9c4938b34e608a25
이정수 <junggri@ijeongsuui-MacBookPro.local>
2020-11-28 00:14:35 +0900

    201128

--- ts/201102.ts
+++ ts/201102.ts
@@ -1,22 +1,28 @@
 interface sq {
   color?: string;
   width?: number;
-  
 }
 
-function createSquare(config: sq): { color: string; area: number }:void {
-  // ...
-
-}
 let squareOptions = { colour: "red", width: 100 };
 let mysq = createSquare(squareOptions);
-
 
 interface SearchFunc {
   (source: string, subString: string): boolean;
 }
 
 interface S {
-  [index:string]:string;
+  [index: string]: string;
 }
 
+function identity<T>(arg: T): T {
+  return arg;
+}
+
+let myIdentity: <T>(arg: T) => T = identity;
+console.log(identity);
+
+function a() {
+  return 1;
+}
+
+let b = a;
